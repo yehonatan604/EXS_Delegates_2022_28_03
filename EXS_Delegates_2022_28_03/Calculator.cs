3dfbd8b70fa2cc6d54e502c84b03ee19d18e9e34
yehonatan604 <97249220+yehonatan604@users.noreply.github.com>
2022-03-28 21:40:06 +0300

    Add project files.

---
+++ EXS_Delegates_2022_28_03/Calculator.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXS_Delegates_2022_28_03
+{
+    public class Calculator
+    {
+        public MethodReturnDoubleAndGetTwoDoubles OnGetResult = null;
+        public double Sum { get; set; }
+
+        public Calculator(string? sign = "")
+        {
+            OnGetResult += AddTwoDoubles;
+            OnGetResult += DivideTwoDoubles;
+            OnGetResult += MultiplyTwoDoubles;
+            OnGetResult += HisurTwoDoubles;
+
+            switch (sign)
+            {
+                case "+":
+                    {
+                        OnGetResult = AddTwoDoubles;
+                        break;
+                    }
+                case "-":
+                    {
+                        OnGetResult = HisurTwoDoubles;
+                        break;
+                    }
+                case "*":
+                    {
+                        OnGetResult = MultiplyTwoDoubles;
+                        break;
+                    }
+                case "/":
+                    {
+                        OnGetResult = DivideTwoDoubles;
+                        break;
+                    }
+            }
+        }
+        
+        public void PrintSum(string msg = "")
+        {
+            Console.WriteLine($"{msg} {Sum}");
+        }
+        public double AddTwoDoubles(double num1, double num2)
+        {
+            Sum = num1 + num2;
+            PrintSum($"{num1}+{num2} =");
+            return Sum;
+        }
+        public double MultiplyTwoDoubles(double num1, double num2)
+        {
+            Sum = num1 * num2;
+            PrintSum($"{num1}*{num2} =");
+            return Sum;
+        }
+        public double DivideTwoDoubles(double num1, double num2)
+        {
+            Sum = num1 / num2;
+            PrintSum($"{num1}/{num2} =");
+            return Sum;
+        }
+        public double HisurTwoDoubles(double num1, double num2)
+        {
+            Sum = num1 - num2;
+            PrintSum($"{num1}-{num2} =");
+            return Sum;
+        }
+    }
+}
---
+++ EXS_Delegates_2022_28_03/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace EXS_Delegates_2022_28_03
+{
+    public delegate int MethodReturnIntAndGetNoParams();
+    public delegate double MethodReturnDoubleAndGetTwoDoubles(double dbl1, double dbl2);
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter 2 numbers to calc:");
+            double dbl1 = Convert.ToDouble(Console.ReadLine());
+            double dbl2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("\n================= with invoke ==============================\n");
+
+            MethodReturnIntAndGetNoParams methodReturnIntAndGetNoParams = GetVoidAndReturnTime;
+            MethodReturnDoubleAndGetTwoDoubles methodReturnDoubleAndGetTwoDoubles = AddTwoDoubles;
+
+            Console.WriteLine("current hour is: " + methodReturnIntAndGetNoParams.Invoke());
+            Console.WriteLine($"{dbl1}+{dbl2}= " + methodReturnDoubleAndGetTwoDoubles.Invoke(dbl1, dbl2) + "\n");
+
+            Console.WriteLine("\n================= no invoke ==============================\n");
+
+            Console.WriteLine("current hour is: " + methodReturnIntAndGetNoParams());
+            Console.WriteLine($"{dbl1}+{dbl2}= " + methodReturnDoubleAndGetTwoDoubles(dbl1, dbl2));
+
+            Console.WriteLine("\n================= calc everything ==============================\n");
+
+            Calculator? calc = new();
+            calc.OnGetResult.Invoke(dbl1, dbl2);
+
+            Console.WriteLine("\n================= calc specific method ==============================\n");
+
+            CalcMethod(calc, "+").Invoke(dbl1, dbl2);
+
+            Console.WriteLine("\n================= calc chosen method ==============================\n");
+
+            Console.WriteLine("please enter calc method:");
+            string? calcuMethodString = Console.ReadLine();
+            calc = new(calcuMethodString);
+            calc.OnGetResult.Invoke(dbl1, dbl2);
+
+
+        }
+        //static methods
+        static MethodReturnDoubleAndGetTwoDoubles CalcMethod(Calculator calc, string sign)
+        {
+            switch (sign)
+            {
+                case "+":
+                    {
+                        return calc.AddTwoDoubles;
+                    }
+                case "-":
+                    {
+                        return calc.HisurTwoDoubles;
+                    }
+                case "/":
+                    {
+                        return calc.DivideTwoDoubles;
+                    }
+                case "*":
+                    {
+                        return calc.MultiplyTwoDoubles;
+                    }
+                default: throw new NotImplementedException();
+            }
+        }
+
+        static double AddTwoDoubles(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+        static int GetVoidAndReturnTime()
+        {
+            return DateTime.Now.Hour;
+        }
+    }
+}
