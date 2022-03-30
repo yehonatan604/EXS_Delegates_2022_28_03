51197285275b75e83fe5275563362b9b82f480d0
yehonatan604 <97249220+yehonatan604@users.noreply.github.com>
2022-03-30 15:15:38 +0300

    added lambdas

--- EXS_Delegates_2022_28_03/Calculator.cs
+++ EXS_Delegates_2022_28_03/Calculator.cs
@@ -9,40 +9,70 @@ namespace EXS_Delegates_2022_28_03
     public class Calculator
     {
         public MethodReturnDoubleAndGetTwoDoubles OnGetResult = null;
+        public Func<double, double, double> calcIt = null;
+        public Action<string> acceptStringForPrintSum = null;
+
         public double Sum { get; set; }
 
         public Calculator(string? sign = "")
         {
+            acceptStringForPrintSum = PrintSum;
+
             OnGetResult += AddTwoDoubles;
             OnGetResult += DivideTwoDoubles;
             OnGetResult += MultiplyTwoDoubles;
             OnGetResult += HisurTwoDoubles;
+
+            calcIt += AddTwoDoubles;
+            calcIt += DivideTwoDoubles;
+            calcIt += MultiplyTwoDoubles;
+            calcIt += HisurTwoDoubles;
 
             switch (sign)
             {
                 case "+":
                     {
                         OnGetResult = AddTwoDoubles;
+                        calcIt = (dbl1, dbl2) =>
+                        {
+                            Console.WriteLine(dbl1 + dbl2);
+                            return dbl1 + dbl2;
+                        };
                         break;
                     }
                 case "-":
                     {
                         OnGetResult = HisurTwoDoubles;
+                        calcIt = (dbl1, dbl2) =>
+                        {
+                            Console.WriteLine(dbl1 - dbl2);
+                            return dbl1 - dbl2;
+                        };
                         break;
                     }
                 case "*":
                     {
                         OnGetResult = MultiplyTwoDoubles;
+                        calcIt = (dbl1, dbl2) =>
+                        {
+                            Console.WriteLine(dbl1 * dbl2);
+                            return dbl1 * dbl2;
+                        };
                         break;
                     }
                 case "/":
                     {
                         OnGetResult = DivideTwoDoubles;
+                        calcIt = (dbl1, dbl2) =>
+                        {
+                            Console.WriteLine(dbl1 / dbl2);
+                            return dbl1 / dbl2;
+                        };
                         break;
                     }
             }
         }
-        
+
         public void PrintSum(string msg = "")
         {
             Console.WriteLine($"{msg} {Sum}");
@@ -50,25 +80,25 @@ namespace EXS_Delegates_2022_28_03
         public double AddTwoDoubles(double num1, double num2)
         {
             Sum = num1 + num2;
-            PrintSum($"{num1}+{num2} =");
+            acceptStringForPrintSum.Invoke($"{num1}+{num2} = ");
             return Sum;
         }
         public double MultiplyTwoDoubles(double num1, double num2)
         {
             Sum = num1 * num2;
-            PrintSum($"{num1}*{num2} =");
+            acceptStringForPrintSum.Invoke($"{num1}*{num2} = ");
             return Sum;
         }
         public double DivideTwoDoubles(double num1, double num2)
         {
             Sum = num1 / num2;
-            PrintSum($"{num1}/{num2} =");
+            acceptStringForPrintSum.Invoke($"{num1}/{num2} = ");
             return Sum;
         }
         public double HisurTwoDoubles(double num1, double num2)
         {
             Sum = num1 - num2;
-            PrintSum($"{num1}-{num2} =");
+            acceptStringForPrintSum.Invoke($"{num1}-{num2} = ");
             return Sum;
         }
     }
--- EXS_Delegates_2022_28_03/Program.cs
+++ EXS_Delegates_2022_28_03/Program.cs
@@ -13,10 +13,10 @@ namespace EXS_Delegates_2022_28_03
             double dbl1 = Convert.ToDouble(Console.ReadLine());
             double dbl2 = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("\n================= with invoke ==============================\n");
-
             MethodReturnIntAndGetNoParams methodReturnIntAndGetNoParams = GetVoidAndReturnTime;
             MethodReturnDoubleAndGetTwoDoubles methodReturnDoubleAndGetTwoDoubles = AddTwoDoubles;
+
+            Console.WriteLine("\n================= with invoke ==============================\n");
 
             Console.WriteLine("current hour is: " + methodReturnIntAndGetNoParams.Invoke());
             Console.WriteLine($"{dbl1}+{dbl2}= " + methodReturnDoubleAndGetTwoDoubles.Invoke(dbl1, dbl2) + "\n");
@@ -31,18 +31,25 @@ namespace EXS_Delegates_2022_28_03
             Calculator? calc = new();
             calc.OnGetResult.Invoke(dbl1, dbl2);
 
+            Console.WriteLine("\n================= calc everything with func & lambda==============================\n");
+
+            calc.calcIt.Invoke(dbl1, dbl2);
+
             Console.WriteLine("\n================= calc specific method ==============================\n");
 
             CalcMethod(calc, "+").Invoke(dbl1, dbl2);
 
+            Console.WriteLine("\n=================  calc specific method - lambda==============================\n");
+
+            calc.calcIt = (dbl1, dbl2) => { Console.WriteLine($"{dbl1} + {dbl2} = {dbl1+dbl2}");
+                                                               return dbl1 + dbl2; };
+            calc.calcIt.Invoke(dbl1, dbl2);
             Console.WriteLine("\n================= calc chosen method ==============================\n");
 
             Console.WriteLine("please enter calc method:");
             string? calcuMethodString = Console.ReadLine();
             calc = new(calcuMethodString);
             calc.OnGetResult.Invoke(dbl1, dbl2);
-
-
         }
         //static methods
         static MethodReturnDoubleAndGetTwoDoubles CalcMethod(Calculator calc, string sign)
